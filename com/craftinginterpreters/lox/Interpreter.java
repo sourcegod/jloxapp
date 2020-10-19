43134535b17526524d96d1d46b4079dd33888d50
Coolbrother <coolbrother@sourcegod.com>
2020-10-19 12:15:15 +0200

    Updated to version: v0.10 - Native Function

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -12,9 +12,27 @@ class LoopState {
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
-  private Environment environment = new Environment();
-  public Environment globalEnv = null;
+  final Environment globals = new Environment();
+  private Environment environment = globals;
+
+
   Stack<LoopState> loopStack = new Stack<>();
+
+  Interpreter() {
+    globals.define("clock", new LoxCallable() {
+      @Override
+      public int arity() { return 0; }
+
+      @Override
+      public Object call(Interpreter interpreter,
+                         List<Object> arguments) {
+        return (double)System.currentTimeMillis() / 1000.0;
+      }
+
+      @Override
+      public String toString() { return "<native fn>"; }
+    });
+  }
 
   void interpret(List<Stmt> statements) {
     try {
